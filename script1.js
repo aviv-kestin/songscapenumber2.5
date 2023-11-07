747143e4f11a19911dd6ee8d6a8d72c261437d0a
aviv-kestin <143558875+aviv-kestin@users.noreply.github.com>
2023-11-07 18:29:00 -0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,28 @@
+function rotateImage() {
+    var image = document.getElementById("image");
+    if (image.style.webkitAnimationPlayState == "running") {
+      image.style.webkitAnimationPlayState = "paused";
+    } else {
+      image.style.webkitAnimationPlayState = "running";
+    }
+  }
+
+
+function ani() {
+var image = document.getElementById('realest').className = 'classname'; 
+if (image.style.webkitAnimationPlayState == "running") {
+  image.style.webkitAnimationPlayState = "paused";
+} 
+
+}
+
+function play() {
+  var audio = document.getElementById("myAudio");
+  audio.play();
+}
+
+
+   
+ 
+
+
